1a5d412720c100cd3b1a06e4c7baf4e042620282
CristianVasquez <cristianvg2003@gmail.com>
2022-01-08 16:59:58 -0500

    Brew formula update for Riakg version v0.3.1

--- Riakg.rb
+++ Riakg.rb
@@ -5,21 +5,21 @@
 class Riakg < Formula
   desc "A command-line interface for the Riak database through its HTTP API"
   homepage "https://github.com/heavyblade/riakg"
-  version "0.3.0"
+  version "0.3.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.0/Riakg_0.3.0_Darwin_arm64.tar.gz"
-      sha256 "e92dbfc3245c526053b17cb15438bb5aa41f613629d42715238ad8297dfcda59"
+      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.1/Riakg_0.3.1_Darwin_arm64.tar.gz"
+      sha256 "0d117a62082936bda9076a577b8b3fb83a7e145e94c9cb11992ce63d5ed04f2c"
 
       def install
         bin.install "Riakg"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.0/Riakg_0.3.0_Darwin_x86_64.tar.gz"
-      sha256 "3dc676e2fa009ad7f7493e44b3c4b09a0f1c6dc84c65a515c6fa08b44792fb0e"
+      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.1/Riakg_0.3.1_Darwin_x86_64.tar.gz"
+      sha256 "242fc83bd2a4feeaf1376db368a39b4718d667041e02ef65c52f901ef0900ffa"
 
       def install
         bin.install "Riakg"
@@ -29,16 +29,16 @@ class Riakg < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.0/Riakg_0.3.0_Linux_arm64.tar.gz"
-      sha256 "76e24cd9bf37e6cef451a1ec88f160c85faec12b3834bf82bae3d5b51ba2abc7"
+      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.1/Riakg_0.3.1_Linux_arm64.tar.gz"
+      sha256 "cacfe7b89c189e1c0a6d14e56843bbc5004d710191cd5de5b8740f4d2406ac0e"
 
       def install
         bin.install "Riakg"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.0/Riakg_0.3.0_Linux_x86_64.tar.gz"
-      sha256 "33c9d3a323d3149007fadd66a0a00883c4d844f07250478978bfd6f5bd3b855c"
+      url "https://github.com/Heavyblade/Riakg/releases/download/v0.3.1/Riakg_0.3.1_Linux_x86_64.tar.gz"
+      sha256 "fb3d1b7541f14df1cc88133c2e01c2e3a9d8cdf910499f4a7f36128726e5a2b2"
 
       def install
         bin.install "Riakg"
